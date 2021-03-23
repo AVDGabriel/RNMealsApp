0ec5be1c3af8a7e9c4dbb1b6788d3ac002702f37
AVDGAbriel <gabriel.avadani97@gmail.com>
2021-03-23 17:37:27 +0200

    configuring the header with navigation options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button, FlatList } from 'react-native';
+import { View, StyleSheet, Text, Button, FlatList, TouchableOpacity } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
-const renderGridItem = (itemData) => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>
-                {itemData.item.title}
-            </Text>
-        </View>
-    );
-};
 
 const CategoriesScreen = props => {
+
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMeals' });
+            }}>
+                <View >
+                    <Text>
+                        {itemData.item.title}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
         //In older versions you have to use the keyExtractor property of the 
         //FlatList in order to not get an error for key.
         <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
     );
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Colors.primaryColor
+    }
 };
 
 const styles = StyleSheet.create({
