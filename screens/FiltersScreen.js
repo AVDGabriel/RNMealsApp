83ffaaaf50676d387596ef7dd93c9697c3d6707d
AVDGAbriel <gabriel.avadani97@gmail.com>
2021-03-25 00:56:44 +0200

    filters screen content

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Switch, Platform } from 'react-native';
+import DefaultText from '../components/DefaultText';
+import Colors from '../constants/Colors';
+
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <DefaultText >
+                {props.label}
+            </DefaultText>
+            <Switch value={props.state}
+                trackColor={{ true: Colors.primaryColor, false: '#ccc' }} s
+                thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+                onValueChange={props.onValueChange} />
+        </View>
+    );
+};
 
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>
-                The filters screen
-        </Text>
+            <DefaultText style={styles.title}>
+                Available Filters / Restrictions
+            </DefaultText>
+            <FilterSwitch
+                label="Gluten-free"
+                state={isGlutenFree}
+                onValueChange={newValue => setIsGlutenFree(newValue)} />
+            <FilterSwitch
+                label="Lactose-free"
+                state={isLactoseFree}
+                onValueChange={newValue => setIsLactoseFree(newValue)} />
+            <FilterSwitch
+                label="Vegan"
+                state={isVegan}
+                onValueChange={newValue => setIsVegan(newValue)} />
+            <FilterSwitch
+                label="Vegetarian"
+                state={isVegetarian}
+                onValueChange={newValue => setIsVegetarian(newValue)} />
         </View>
     );
 };
@@ -14,8 +51,20 @@ const FiltersScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        marginVertical: 10
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
     }
 });
 
